e80d00fd09198f10952d968e52cb010a2e3a5479
weststar25 <jiwoo932@naver.com>
2019-01-30 13:33:35 +0900

    [ADD] Add .gitignore

---
+++ GithubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by 김지우 on 2019. 1. 30..
+//  Copyright © 2019년 havetherain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
